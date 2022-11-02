601973bb8b24e42dd77595b8648369b011c7efd4
goder1 <113771449+goder1@users.noreply.github.com>
2022-11-02 18:33:10 +0300

    Determinant added (+visual decorations)
    Updated version of 2 previous commits

--- Matrix.h
+++ Matrix.h
@@ -10,200 +10,250 @@ using namespace std;
 
 template<typename T>
 class Matrix{
-T** matrix;
-int a;
-int b;
+    T** matrix;
+    int a;
+    int b;
 
 public:
- void set_a(int a)
- {
-     this->a = a;
- }
-
- int get_a()
- {
-     return a;
- }
-
- int get_b()
- {
-     return b;
- }
- void set_b(int b)
- {
-     this->b = b;
- }
-
- Matrix()
- {
-     a = 0;b = 0;
- }
-
- Matrix (int x)
- {
-     a = x; b = x;
- }
-
- Matrix (int x, int y)
- {
-     a = x; b = y;
- }
-
- Matrix E_matrix(int a, int b)
-{
-    Matrix A;
-    A.matrix = new T* [a];
-    for(int i = 0; i<a; i++)
+    void set_a(int a)
     {
-        A.matrix[i] = new T [b];
+        this->a = a;
     }
-    for(int i = 0 ;i<a;i++)
-        for(int j = 0;j<b;j++)
+
+    int get_a()
+    {
+        return a;
+    }
+
+    int get_b()
+    {
+        return b;
+    }
+    void set_b(int b)
+    {
+        this->b = b;
+    }
+
+    Matrix()
+    {
+        a = 0;b = 0;
+    }
+
+    Matrix(int x)
+    {
+        a = x; b = x;
+    }
+
+    Matrix(int x, int y)
+    {
+        a = x; b = y;
+    }
+
+    Matrix E_matrix(int a, int b)
+    {
+        Matrix A;
+        A.matrix = new T* [a];
+        for(int i = 0; i < a; i++)
         {
-            if(i == j)A.matrix[i][j]=1;
-            else A.matrix[i][j]=0;
+            A.matrix[i] = new T [b];
         }
-    A.a = a;
-    A.b = b;
-    return A;
-}
-
-
-
-Matrix umnozim(Matrix B)
-{
-if(b!=B.a) cout << "0";//throw string("Wrong size of matrices");
-else {
-Matrix C;
-C.a = a;
-C.b = B.b;
-C.matrix = new T* [a];
-for(int i = 0;i<a;i++)
-    C.matrix[i] = new T [B.b];
-
-    for(int i = 0; i < a; i++)
-    for(int j = 0;j<B.b;j++)
-    C.matrix[i][j]=0;
-for(int i = 0;i<a;i++)
-    for(int q = 0;q<B.b;q++)
-    {
-        for(int t = 0;t<b;t++)
-        C.matrix[i][q]+=matrix[i][t]*B.matrix[t][q];
-    }
-return C;
-}
-Matrix<int> A(1,1);
-return A;
-}
-
-Matrix fastPOW(int n){
-    if(n==0)return E_matrix(a,b);
-    else if(n%2==0)
-    {
-        Matrix c;
-        c.a = a;
-        c.b = b;
-        c.matrix = matrix;
-        return c.fastPOW(n/2).umnozim(c.fastPOW(n/2));
-    }
-    else
-    {
-        Matrix c;
-        c.a = a;
-        c.b = b;
-        c.matrix = matrix;
-        return c.umnozim(c.fastPOW(n/2).umnozim(c.fastPOW(n/2)));
-    }
-}
-
-
-Matrix transponiruy()
-{
-    Matrix C;
-    C.a = b;
-    C.b = a;
-    C.matrix = new T* [C.a];
-    for(int i = 0; i < C.a ; i++)C.matrix[i] = new T [C.b];
-    for (int i = 0; i < b; i++)
+        for(int i = 0 ; i < a; i++)
         {
-            for(int q = 0; q < a; q++)
+            for(int j = 0; j < b; j++)
             {
-                C.matrix[i][q]=matrix[q][i];
+                if(i == j)A.matrix[i][j] = 1;
+                else A.matrix[i][j] = 0;
+            }
+        }
+        A.a = a;
+        A.b = b;
+        return A;
+    }
+
+
+
+    Matrix operator*(Matrix B)
+    {
+        Matrix C;
+        C.a = a;
+        C.b = B.b;
+        C.matrix = new T* [a];
+        for(int i = 0; i < a; i++)
+        {
+            C.matrix[i] = new T [B.b];
+        }
+        for(int i = 0; i < a; i++)
+        {
+            for(int j = 0; j < B.b; j++)
+            {
+                C.matrix[i][j]=0;
+            }
+        }
+        for(int i = 0; i < a; i++)
+        {
+            for(int j = 0; j < B.b; j++)
+            {
+                for(int t = 0; t < b; t++)
+                C.matrix[i][j] += matrix[i][t] * B.matrix[t][j];
             }
         }
         return C;
-}
+    }
 
-Matrix slozhi(Matrix B){
-    Matrix C;
-    C.a = a;
-    C.b = b;
-    C.matrix = new T* [C.a];
-    for(int i = 0; i < C.a ; i++)C.matrix[i] = new T [C.b];
-    for (int i = 0; i < a; i++)
+    Matrix fastPOW(int n)
     {
-        for(int q = 0; q < b; q++)
+        if(n == 0)return E_matrix(a,b);
+        else if(n%2 == 0)
         {
-            C.matrix[i][q]=matrix[i][q]+B.matrix[i][q];
+            Matrix C;
+            C.a = a;
+            C.b = b;
+            C.matrix = new T* [a];
+            for(int i = 0; i < a; i++)
+            {
+                C.matrix[i] = new T [b];
+            }
+            for(int i = 0; i < a; i++)
+            {
+                for(int j = 0; j < b; j++)
+                {
+                    C.matrix[i][j] = matrix[i][j];
+                }
+            }
+            //c.matrix = matrix;
+            return C.fastPOW(n/2) * C.fastPOW(n/2);
+        }
+        else
+        {
+            Matrix C;
+            C.a = a;
+            C.b = b;
+            C.matrix = new T* [a];
+            for(int i = 0; i < a; i++)
+            {
+                C.matrix[i] = new T [b];
+            }
+            for(int i = 0; i < a; i++)
+            {
+                for(int j = 0; j < b; j++)
+                {
+                    C.matrix[i][j] = matrix[i][j];
+                }
+            }
+            //c.matrix = matrix;
+            return C * (C.fastPOW(n/2) * C.fastPOW(n/2));
         }
     }
-    return C;
-}
 
-Matrix umnozh(T k){
-    Matrix C;
-    C.a = a;
-    C.b = b;
-    C.matrix = matrix;
-    for (int i = 0; i < a; i++)
+
+    Matrix transposition()
     {
-        for(int q = 0; q < b; q++)
+        Matrix C;
+        C.a = b;
+        C.b = a;
+        C.matrix = new T* [C.a];
+        for(int i = 0; i < C.a ; i++)
         {
-            C.matrix[i][q]*=k;
+            C.matrix[i] = new T [C.b];
+        }
+        for (int i = 0; i < b; i++)
+        {
+            for(int j = 0; j < a; j++)
+            {
+                C.matrix[i][j] = matrix[j][i];
+            }
+        }
+        return C;
+    }
+
+    Matrix operator+(Matrix B)
+    {
+        Matrix C;
+        C.a = a;
+        C.b = b;
+        C.matrix = new T* [C.a];
+        for(int i = 0; i < C.a ; i++)
+        {
+            C.matrix[i] = new T [C.b];
+        }
+        for (int i = 0; i < a; i++)
+        {
+            for(int j = 0; j < b; j++)
+            {
+                C.matrix[i][j] = matrix[i][j] + B.matrix[i][j];
+            }
+        }
+        return C;
+    }
+
+    Matrix operator*(T k)
+    {
+        Matrix C;
+        C.a = a;
+        C.b = b;
+        //C.matrix = matrix;
+        C.matrix = new T* [a];
+        for(int i = 0; i < a; i++)
+        {
+            C.matrix[i] = new T [b];
+        }
+        for(int i = 0; i < a; i++)
+        {
+            for(int j = 0; j < b; j++)
+            {
+                C.matrix[i][j] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < a; i++)
+        {
+            for(int j = 0; j < b; j++)
+            {
+                C.matrix[i][j] *= k;
+            }
+        }
+        return C;
+    }
+
+    int determinant(int a)
+    {
+        if(a==1)return matrix[0][0];
+        else if(a==2)return matrix[0][0]*matrix[1][1]-matrix[1][0]*matrix[0][1];
+        else
+        {
+            int k = 1,s = 0;
+            for(int i = 0; i < a; i++)
+            {
+                Matrix<int> B(a-1);
+                B.matrix = new int* [a-1];
+                for (int j = 0; j < a-1; j++)
+                {
+                    B.matrix[j] = new int [a-1];
+                }
+                for (int j = 0; j < a-1; j++)
+                {
+                for(int q = 0; q < i; q++)
+                {
+                    B.matrix[j][q] = matrix[j+1][q];
+                }
+                for(int q = i; q < a-1; q++)
+                {
+                    B.matrix[j][q] = matrix[j+1][q+1];
+                }
+                }
+                s += matrix[0][i] * B.determinant(a-1) * k;
+                k =- k;
+            }
+        return s;
         }
     }
-    return C;
-}
 
-int determinant(int a)
-{
-if(a==1)return matrix[0][0];
-else if(a==2)return matrix[0][0]*matrix[1][1]-matrix[1][0]*matrix[0][1];
-else {
-    int k = 1,s = 0;
-    for(int i=0;i<a;i++)
+    template<typename Type> friend istream& operator>>(istream&, Matrix<Type>&);
+    template<typename Type> friend ostream& operator<<(ostream&, const Matrix<Type>&);
+
+    T* operator[](int a)
     {
-        Matrix<int> B(a-1);
-        B.matrix = new int* [a-1];
-        for (int j = 0; j < a-1; j++) B.matrix[j] = new int [a-1];
-        for (int j = 0; j < a-1; j++)
-        {
-        for(int q = 0; q < i; q++)
-        {
-        B.matrix[j][q] = matrix[j+1][q];
-        }
-        for(int q = i; q < a-1; q++)
-        {
-        B.matrix[j][q] = matrix[j+1][q+1];
-        }
-        }
-    s+=matrix[0][i]*B.determinant(a-1)*k;
-    k=-k;
-
+        return matrix[a];
     }
-    return s;
-
-
-}
-}
-
-template<typename Type> friend istream& operator>>(istream&, Matrix<Type>&);
-template<typename Type> friend ostream& operator<<(ostream&, const Matrix<Type>&);
-
-T* operator[](int a)
-{
-    return matrix[a];
-}
 
 
 };
@@ -214,7 +264,9 @@ ostream& operator<<(ostream& out, const Matrix<T>& A)
     for (int i = 0; i < A.a; i++)
     {
         for(int j = 0; j < A.b; j++)
-        out << A.matrix[i][j] << " ";
+        {
+            out << A.matrix[i][j] << " ";
+        }
         out << endl;
     }
     return out;
@@ -223,11 +275,16 @@ ostream& operator<<(ostream& out, const Matrix<T>& A)
 template<typename T>
 istream& operator>>(istream& in, Matrix<T>& A) {
     A.matrix = new T* [A.a];
-    for(int i = 0; i < A.a ; i++)A.matrix[i] = new T [A.b];
+    for(int i = 0; i < A.a ; i++)
+    {
+        A.matrix[i] = new T [A.b];
+    }
     for (int i = 0; i < A.a; i++)
     {
         for(int j = 0; j < A.b; j++)
+        {
         in >> A.matrix[i][j];
+        }
     }
     return in;
 }
--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ using namespace std;
 
 void strange_number()
 {
-cout<<"Strange number, don't you think?\n";
+cout << "Strange number, don't you think?\n";
 sleep(1);
 system("cls");
 }
 
 bool check_number(string a)
 {
-    int er=0;
-    for (int i = 0;i<a.length();i++)
+    int er = 0;
+    for (int i = 0; i < a.length(); i++)
     {
-        if(int(a[i])-48<0||int(a[i])-48>9){er=1;i=a.length();}
+        if(int(a[i]) - 48 < 0 || int(a[i]) - 48 > 9){er = 1;i = a.length();}
     }
     if(er)return false;
     else return true;
@@ -29,182 +29,176 @@ bool check_number(string a)
 
 int main()
 {
-srand(time(NULL));
-int g=0,p;
-int a,b,c,d;
-string w;
-string command ="";
-while(command != "STOP")
-{
-cout<<"1.Multiplying matrix by number \n2.Sum of matrix\n3.Transposition of matrix\n4.Multiplying matrices\n5.Matrix degree\n6.Matrix determinant\n7.STOP\n\n Choose the command:\n";
-cin>>w;
-if(int(w[0])-48==1&&w.size()==1){
-cout<<"Enter the size of matrix\n";
-try
-{
-cin >> a;
-if(a<=0)throw "gg";
-cin >> b;
-if(b<=0)throw "gg";
-Matrix<int> A(a,b);
-cout<<"Enter the numbers for matrix\n";
-cin >> A;
-cout<<"Enter the number for multiplying\n";
-int k;cin>>k;
-if(k==0)throw "gg";
-cout<<"Original matrix:\n";
-cout<<A;
-Matrix<int> C = A.umnozh(k);
-cout<<"\nNew matrix:\n";
-cout<<C;
-cout<<endl;
-}
-catch(const char *err)
-{
-    if(err == "gg")cout<<"Strange number :(\n";
-}
-}
+    srand(time(NULL));
+    int a,b,c,d;
+    string w;
+    string command = "";
+    while(command != "STOP")
+    {
+        cout << "1.Multiplying matrix by number \n2.Sum of matrix\n3.Transposition of matrix\n4.Multiplying matrices\n5.Matrix degree\n6.Matrix determinant\n7.STOP\n\n Choose the command:\n";
+        cin >> w;
+        if(int(w[0]) - 48 == 1 && w.size() == 1)
+        {
+            cout << "Enter the size of matrix\n";
+            try
+            {
+                cin >> a;
+                if(a <= 0)throw "gg";
+                cin >> b;
+                if(b <= 0)throw "gg";
+                Matrix<int> A(a,b);
+                cout << "Enter the numbers for matrix\n";
+                cin >> A;
+                cout << "Enter the number for multiplying\n";
+                int k;
+                cin >> k;
+                if(k==0)throw "gg";
+                cout << "Original matrix:\n";
+                cout << A;
+                Matrix<int> C = A * k;
+                cout << "\nNew matrix:\n";
+                cout << C;
+                cout << endl;
+            }
+            catch(const char *err)
+            {
+                if(err == "gg")cout << "Strange number :(\n";
+            }
+        }
 
 
-else if(int(w[0])-48==2&&w.size()==1)
-{
+        else if(int(w[0])-48==2&&w.size()==1)
+        {
 
-cout<<"Enter the size of matrices\n";
-cin >> a >> b;
-Matrix<int> A(a,b),B(a,b);
-cout<<"Enter the numbers for the first matrix\n";
-cin >> A;
-cout<<"Enter the numbers for the second matrix\n";
-cin >> B;
-cout<<"Original matrices:\n\n";
-cout<< A << endl << B;
-cout<<endl;
+            cout << "Enter the size of matrices\n";
+            cin >> a >> b;
+            Matrix<int> A(a,b),B(a,b);
+            cout << "Enter the numbers for the first matrix\n";
+            cin >> A;
+            cout << "Enter the numbers for the second matrix\n";
+            cin >> B;
+            cout << "Original matrices:\n\n";
+            cout << A << endl << B;
+            cout << endl;
 
-Matrix<int> C;
-C = A.slozhi(B);
-cout<<"New matrix:\n"<<C;
-}
-
-
-else if(int(w[0])-48==3&&w.size()==1){
-cout<<"Enter the size of matrix\n";
-cin >> a >> b;
-Matrix<int> A;
-A.set_a(a);
-A.set_b(b);
-//cout<<A.get_a()<<endl;
-cout<<"Enter the numbers for the matrix\n";
-cin >> A;
-cout<<"Original matrix:\n";
-cout << A;
-Matrix<int> C = A.transponiruy();
-cout<<"\nNew matrix:\n"<<C;
-}
+            Matrix<int> C;
+            C = A + B;
+            cout << "New matrix:\n"<<C;
+        }
 
 
-else if(int(w[0])-48==4&&w.size()==1){
-cout<<"Enter the size of the first matrix\n";
-cin >> a >> b;
-Matrix<int> A;
-A.set_a(a);
-A.set_b(b);
-cout<<"Enter the numbers for the first matrix\n";
-cin >> A;
-cout<<"Enter the size of the second matrix\n";
-cin >> c >> d;
-
-Matrix<int> B;
-B.set_a(c);
-B.set_b(d);
-cout<<"Enter the numbers for the second matrix\n";
-cin >> B;
-cout<<"Original matrices:\n\n";
-cout<<A<<endl<<B;
-Matrix<int> C;
-try{
-C = A.umnozim(B);
-cout<<"\nNew matrix:\n";
-cout<<C;
-}
-catch(string error) {
-cout<<"Error: "<<error<<endl;
-}
+        else if(int(w[0]) - 48 == 3 && w.size() == 1)
+        {
+            cout << "Enter the size of matrix\n";
+            cin >> a >> b;
+            Matrix<int> A;
+            A.set_a(a);
+            A.set_b(b);
+            cout << "Enter the numbers for the matrix\n";
+            cin >> A;
+            cout << "Original matrix:\n";
+            cout << A;
+            Matrix<int> C = A.transposition();
+            cout << "\nNew matrix:\n" << C;
+        }
 
 
-}
+        else if(int(w[0]) - 48 == 4 && w.size() == 1)
+        {
+            cout << "Enter the size of the first matrix\n";
+            cin >> a >> b;
+            Matrix<int> A;
+            A.set_a(a);
+            A.set_b(b);
+            cout << "Enter the numbers for the first matrix\n";
+            cin >> A;
+            cout << "Enter the size of the second matrix\n";
+            cin >> c >> d;
+
+            Matrix<int> B;
+            B.set_a(c);
+            B.set_b(d);
+            cout << "Enter the numbers for the second matrix\n";
+            cin >> B;
+            cout << "Original matrices:\n\n";
+            cout << A << endl << B;
+            Matrix<int> C;
+            try
+            {
+                C = A * B;
+                cout << "\nNew matrix:\n";
+                cout << C;
+            }
+            catch(string error)
+            {
+                cout << "Error: " << error << endl;
+            }
 
 
-else if(int(w[0])-48==5&&w.size()==1)
-{
-cout<<"Enter the size of matrix\n";
-cin >> a >> b;
-Matrix<int> A;
-A.set_a(a);
-A.set_b(b);
-cout<<"Enter the numbers for the matrix\n";
-cin >> A;
-if(a==b)
-{
-cout<<"Enter the degree of matrix\n";
-int n;
-cin >> n;
-cout<<"Original matrix:\n"<<A;
-
-cout<<"Matrix in "<<n<<" degree:\n";
-Matrix<int> B;
-
-B = A.fastPOW(n);
-cout<<B;
-}
-else cout<<"This matrix could not be multiplied by itself :/";
-}
+        }
 
 
-else if(int(w[0])-48==6&&w.size()==1)
-{
-cout<<"Enter the size of matrix\n";
-cin >> a;
-Matrix<int> A;
-A.set_a(a);
-A.set_b(a);
-cout<<"Enter the numbers for the matrix\n";
-cin >> A;
-cout<<"Matrix:\n"<<A;
+        else if(int(w[0]) - 48 == 5 && w.size() == 1)
+        {
+            cout << "Enter the size of matrix\n";
+            cin >> a >> b;
+            Matrix<int> A;
+            A.set_a(a);
+            A.set_b(b);
+            cout << "Enter the numbers for the matrix\n";
+            cin >> A;
+            if(a == b)
+            {
+                cout << "Enter the degree of matrix\n";
+                int n;
+                cin >> n;
+                Matrix<int> B;
+                B = A.fastPOW(n);
+                cout << "Original matrix:\n" << A;
+                cout << "Matrix in " << n << " degree:\n";
+                cout << B;
+            }
+            else cout << "This matrix could not be multiplied by itself :/";
+        }
 
-cout<<"determinant:\n";
-int det=0;
+        else if(int(w[0]) - 48 == 6 && w.size() == 1)
+        {
+            cout << "Enter the size of matrix (only one number)\n";
+            cin >> a;
+            Matrix<int> A;
+            A.set_a(a);
+            A.set_b(a);
+            cout << "Enter the numbers for the matrix\n";
+            cin >> A;
+            cout << "Matrix:\n" << A;
+            cout << "determinant:\n";
+            int det = 0;
+            det = A.determinant(a);
+            cout << det << endl;
+        }
 
-det = A.determinant(a);
-cout<<det<<endl;
-}
+
+        else if(int(w[0]) - 48 == 7 && w.size() == 1)
+        {
+            command = "STOP";
+            return 0;
+        }
 
 
-
-else if(int(w[0])-48==7&&w.size()==1)
-{
-    command = "STOP";
+        else
+        {
+            cout << "strange command :/\n\n";
+        }
+        cout << "Do you want to continue?\nEnter 'continue'\n";
+        string cont = "";
+        cin >> cont;
+        if(cont == "continue")system("cls");
+        else
+        {
+            cout << "indeed )))\n";
+            sleep(1);
+            system("cls");
+        }
+    }
     return 0;
-}
-
-
-else
-{
-    cout<<"strange command :/\n\n";
-}
-cout<<"Do you want to continue?\nEnter 'continue'\n";
-string cont = "";
-cin >> cont;
-if(cont == "continue")
-system("clear");
-else
-{
-    cout << "indeed )))\n";
-    sleep(1);
-    system("clear");
-
-}
-
-}
-
-return 0;
 }
